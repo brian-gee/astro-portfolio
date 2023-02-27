c3b5ae5c2fdef42b72fdf1b86265dfe7f277bace
Brian Geertsma <briange@tuta.io>
2023-02-27 01:05:16 -0500

    adding more books

--- src/content/books.ts
+++ src/content/books.ts
@@ -1,4 +1,7 @@
-import miniHabits from "@assets/mini-habits-opt.jpg";
+import miniHabits from "@assets/books/mini-habits.jpg";
+import howToWinFriends from "@assets/books/how-to-win-friends.jpg";
+import howToBeAnImperfectionist from "@assets/books/imperfectionist.jpg";
+import soundtracks from "@assets/books/soundtracks.jpg";
 
 export const books = [
   {
@@ -8,6 +11,36 @@ export const books = [
       "Great explanation of why habits are so important and how to turn small actions into big results.",
     img: miniHabits,
     year: 2013,
+    reading: false,
+    loading: "eager",
+  },
+  {
+    title: "How to Win Friends and Influence People",
+    author: "Dale Carnegie",
+    comment:
+      "Great explanation of why habits are so important and how to turn small actions into big results.",
+    img: howToWinFriends,
+    year: 1936,
+    reading: false,
+    loading: "eager",
+  },
+  {
+    title: "Soundtracks: The Surprising Solution to Overthinking",
+    author: "Jon Acuff",
+    comment:
+      "An execllent book about how to stop overthinking and start living your life. It's a great book for anyone who struggles with anxiety.",
+    img: soundtracks,
+    year: 2021,
+    reading: false,
+    loading: "eager",
+  },
+  {
+    title:
+      "How to Be an Imperfectionist: The New Way to Fearlessness, Confidence, and Freedom from Perfectionism",
+    author: "Stephen Guise",
+    comment: "",
+    img: howToBeAnImperfectionist,
+    year: 2015,
     reading: false,
     loading: "eager",
   },
